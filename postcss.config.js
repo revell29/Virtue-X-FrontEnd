0ae8de62a6f6e3290448830d687e76dab04b91da
revell29 <diraapsya0@gmail.com>
2020-09-16 17:54:19 +0700

    fix postcss tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     ...(process.env.NODE_ENV === "production"
       ? {
           "@fullhuman/postcss-purgecss": {
-            content: ["./components/**/*.js", "./pages/**/*.js"],
+            content: ["./src/components/**/*.tsx", "./src/pages/**/*.tsx"],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
           },
